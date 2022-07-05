afe4f2ce4e93bdeb754617322f875be65abf0298
AhmedNabilTopaz <60816134+AhmedNabilTopaz@users.noreply.github.com>
2022-07-05 16:02:15 +0200

    Initial commit

---
+++ stock_topaz/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Topaz Inventory',
+    'version': '1.0',
+    'summary': 'Editing Stock Locations ',
+    'description': "",
+    'website': 'https://www.odoo.com/app/inventory',
+    'depends': ['stock'],
+    'sequence': -100,
+    'data': [
+    ],
+
+}
